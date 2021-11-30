23d2b3b2f31f1055cacc0f2766a9cae32b5a6d47
FrozenBurrito <f3rm3ndouza@outlook.com>
2021-11-30 11:46:34 -0600

    agregar página de inicio y config de tema

--- app/src/main/java/com/example/proyectomovilfinal/MainActivity.java
+++ app/src/main/java/com/example/proyectomovilfinal/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.proyectomovilfinal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragmentoPagina = new PaginaInicio();
+
+        if(savedInstanceState == null && findViewById(R.id.fragmento_pagina) != null)
+        {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragmento_pagina, fragmentoPagina, null)
+                    .commit();
+        }
     }
 }
